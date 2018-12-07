87dc094ccbfa062a4c2d55de3b45bb6f253039d2
zhangzhongxiang <zhangzhongxiang@100tal.com>
2018-12-07 19:17:04 +0800

    first commit

---
+++ config.tcl
@@ -0,0 +1,24 @@
+## server configs
+## hostname    "127.0.0.1"
+## port        "22"
+## username    "root"
+## password    "toor"
+## idfile      "ids/1234.pem"
+## passphrase  ""
+
+# example.me
+dict set servers example.me hostname    "127.0.0.1"
+dict set servers example.me port        "22"
+dict set servers example.me username    "root"
+dict set servers example.me password    "toor"
+dict set servers example.me idfile      ""
+dict set servers example.me passphrase  ""
+
+
+# example.tbj
+dict set servers example.tbj hostname    "127.0.0.1"
+dict set servers example.tbj port        "22"
+dict set servers example.tbj username    "root"
+dict set servers example.tbj password    ""
+dict set servers example.tbj idfile      "ids/example.pem"
+dict set servers example.tbj passphrase  "toor"
